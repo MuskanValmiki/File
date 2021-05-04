0fd4d693a93c06a76518412ad6c8c3a84fb5028c
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-05-04 15:49:02 +0530

    Add files via upload

---
+++ kashish.py
@@ -0,0 +1,19 @@
+f=open("kashish.txt","r")
+f1=f.read()
+#print(f1)
+#print(type(f1))
+a=list(f1)
+#print(a)
+
+
+#print(type(a))
+# i=0
+# while i<len(f1):
+#     if f1[i] not in ",":
+#         print(f1[i],end="")
+#     i=i+1
+# f.close()
+# for i in a:
+#     print(i,end="")
+
+        
